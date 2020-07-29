61cf8b6a833c93f7eb2868f667070712bfcbd6c2
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-07-29 19:48:28 +0300

    Added controllers

---
+++ src/main/java/com/softserve/academy/controller/MarathonController.java
@@ -0,0 +1,89 @@
+package com.softserve.academy.controller;
+
+import com.softserve.academy.dto.MarathonDto;
+import com.softserve.academy.dto.UserDto;
+import com.softserve.academy.exception.MarathonNotFoundException;
+import com.softserve.academy.service.MarathonService;
+import com.softserve.academy.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping(value = {"/", "/marathons"})
+public class MarathonController {
+    @Autowired
+    MarathonService marathonService;
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping()
+    public String showAllMarathons(Model model) {
+        model.addAttribute("marathons", marathonDtoSorting(marathonService.getAll()));
+        return "marathons";
+    }
+
+    @GetMapping("/add")
+    public String showCreateMarathonPage(Model model) {
+        if (!model.containsAttribute("marathon")) {
+            model.addAttribute("marathon", new MarathonDto());
+        }
+        model.addAttribute("allStudents", userDtoSorting(userService.getAll()));
+        return "marathonEdit";
+    }
+
+    @PostMapping("/add")
+    public String addMarathon(@Valid @ModelAttribute("marathon") MarathonDto marathon,
+                              BindingResult bindingResult,
+                              RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.marathon", bindingResult);
+            redirectAttributes.addFlashAttribute("marathon", marathon);
+            return "redirect:/marathons" + (marathon.getId() > 0 ? "/edit/" + marathon.getId() : "/add");
+        }
+
+        marathonService.createOrUpdate(marathon);
+        return "redirect:/marathons";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editCurrentMarathon(@PathVariable("id") long id, Model model, RedirectAttributes redirectAttributes) {
+        if (!model.containsAttribute("marathon")) {
+            try {
+                model.addAttribute("marathon", marathonService.getMarathonById(id));
+            } catch (MarathonNotFoundException e) {
+                redirectAttributes.addFlashAttribute("error", "Marathon with given id doesn`t exist!");
+                return "redirect:/marathons";
+            }
+        }
+        model.addAttribute("allStudents", userDtoSorting(userService.getAll()));
+        return "marathonEdit";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteMarathonById(@PathVariable("id") long id) {
+        marathonService.deleteMarathonById(id);
+        return "redirect:/marathons";
+    }
+
+    private List<MarathonDto> marathonDtoSorting(List<MarathonDto> list) {
+        return list.stream()
+                .sorted(Comparator.comparing(MarathonDto::getTitle))
+                .collect(Collectors.toList());
+    }
+
+    private List<UserDto> userDtoSorting(List<UserDto> list) {
+        return list.stream()
+                .sorted(Comparator.comparing(UserDto::getLastName).thenComparing(UserDto::getFirstName))
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/com/softserve/academy/controller/UserController.java
@@ -0,0 +1,90 @@
+package com.softserve.academy.controller;
+
+import com.softserve.academy.dto.UserDto;
+import com.softserve.academy.exception.UserNotFoundException;
+import com.softserve.academy.model.User;
+import com.softserve.academy.service.MarathonService;
+import com.softserve.academy.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+
+@Controller
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    MarathonService marathonService;
+
+    @GetMapping("/students")
+    public String showAllUsersPage(Model model) {
+        model.addAttribute("students", userService.getAllByRole(User.Role.TRAINEE));
+        return "students";
+    }
+
+    @GetMapping("/students/{id}")
+    public String showMarathonUsersPage(@PathVariable("id") long id, Model model) {
+        var marathon = marathonService.getMarathonById(id);
+        model.addAttribute("students", marathon.getUsers());
+        model.addAttribute("marathon", marathon);
+        return "students";
+    }
+
+    @GetMapping("/students/add")
+    public String showCreateUserPage(Model model) {
+        if (!model.containsAttribute("user")) {
+            var user = new UserDto();
+            user.setRole(User.Role.TRAINEE);
+            model.addAttribute("user", user);
+        }
+        model.addAttribute("allMarathons", marathonService.getAll());
+        return "studentEdit";
+    }
+
+    @GetMapping("/students/edit/{id}")
+    public String showEditUserPage(@PathVariable("id") long id, Model model, RedirectAttributes redirectAttributes) {
+        if (!model.containsAttribute("user")) {
+            try {
+                model.addAttribute("user", userService.getUserByIdFetchMarathons(id));
+            } catch (UserNotFoundException e) {
+                redirectAttributes.addFlashAttribute("error", "User with given id not found!");
+                return "redirect:/students";
+            }
+        }
+        model.addAttribute("allMarathons", marathonService.getAll());
+        return "studentEdit";
+    }
+
+    @GetMapping("/students/delete/{id}")
+    public String deleteUserById(@PathVariable("id") long id) {
+        userService.userDelete(id);
+        return "redirect:/students";
+    }
+
+    @GetMapping("/students/{mid}/delete/{id}")
+    public String removeUserFromMarathon(@PathVariable("id") long id, @PathVariable("mid") long mid) {
+        marathonService.removeFromMarathon(id, mid);
+        return "redirect:/students/" + mid;
+    }
+
+    @PostMapping("/students/update")
+    public String createOrUpdateUser(@Valid @ModelAttribute("user") UserDto user, BindingResult result,
+                                     RedirectAttributes redirectAttributes) {
+        if (result.hasErrors()) {
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.user", result);
+            redirectAttributes.addFlashAttribute("user", user);
+            return "redirect:/students/" + (user.getId() > 0 ? "edit/" + user.getId() : "add");
+        }
+        userService.createOrUpdateUser(user);
+        return "redirect:/students";
+    }
+}
